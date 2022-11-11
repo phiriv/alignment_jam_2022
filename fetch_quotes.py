cf9f3c2d93af0085693f541aac70e285180a7314
Giles Edkins <edkins@gmail.com>
2022-11-11 14:20:05 -0500

    Wikiquote fetcher

---
+++ fetch_quotes.py
@@ -0,0 +1,17 @@
+import wikiquotes
+import json
+import os
+
+results = []
+quotes = wikiquotes.get_quotes('war','english')
+for quote in quotes:
+    results.append({
+        'text': quote,
+        'about_war': 1,
+        'source': 'wikiquotes'
+    })
+
+os.makedirs('data', exist_ok=True)
+with open('data/war_wq.json','w') as f:
+    json.dump(results, f, indent=True)
+
